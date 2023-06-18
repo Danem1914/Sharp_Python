1a1cc4d26b1d8381ddab2c9e3a5cd43af1465892
Daniel <shomewutugot@gmail.com>
2023-06-17 21:40:38 -0400

    scratch folder

--- EasyPythonGame.py
+++ EasyPythonGame.py
@@ -2,4 +2,11 @@
 
 import arcade
 
-print("Hello Brodie.")
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "LIL TIKES FIRST PYTHON PROJECT"
+
+class Mygame(arcade.Window):
+    """
+    Main application class
+    """
